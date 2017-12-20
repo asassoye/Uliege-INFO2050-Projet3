381204253ff706ae92b87875a8f831f709cecc04
asassoye <andrew.sassoye@student.ulg.ac.be>
2017-12-20 18:16:41 +0100

    initial wrapImages

---
+++ Code/Comic.c
@@ -0,0 +1,10 @@
+#include "Comic.h"
+
+
+/*
+ * Je m'occupe de celui la :)
+ */
+size_t* wrapImages(const PNMImage** images, size_t nbImages, size_t comicWidth, size_t comicBorder){
+
+    return 0;
+}

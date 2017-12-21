1cb71fd7ff18b037ac2080ec6a9b675ffaf7673a
asassoye <andrew.sassoye@student.ulg.ac.be>
2017-12-21 14:49:22 +0100

    Merge branch 'master' of /Users/andrew.sassoye/Documents/Etudes/2017-18/Q1/[INFO2050-1] Programmation avancée/Projets/Projet 3 with conflicts.

--- Code/Comic.c
+++ Code/Comic.c
@@ -4,8 +4,7 @@
 /*
  * Je m'occupe de celui la :)
  */
-size_t *
-wrapImages(const PNMImage **images, size_t nbImages, size_t comicWidth, size_t comicBorder) {
+size_t* wrapImages(const PNMImage** images, size_t nbImages, size_t comicWidth, size_t comicBorder){
 
     return 0;
 }
